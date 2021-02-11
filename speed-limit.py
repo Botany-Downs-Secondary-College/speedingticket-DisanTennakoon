947700ec7e5973ab451234124f23f4776feab34d
DisanTennakoon <78841293+DisanTennakoon@users.noreply.github.com>
2021-02-12 10:41:38 +1300

    Update speed-limit.py

--- speed-limit.py
+++ speed-limit.py
@@ -1,7 +1,8 @@
+list =["Ryan, Prasad, Ali, Jake Paul"] 
 speed_limit = 100
-difference = speed - speed_limit
-speed = int(input("How fast were you driving?: "))
 driver = input("What is your name?: ")
+speed = int(input("How fast were you driving?: "))
+difference = speed - speed_limit
 if speed <= 100:
     print("Good job {}, you are not speeding.".format(driver))
 elif speed > 100 and speed <150:
@@ -15,6 +16,8 @@ elif speed >= 150 and speed <=180:
     print("{}, you have sped {}km/h over the limit. May I see your license? Sike, it's mine now! Go back to driving school!".format(driver, difference))
 elif speed > 180 and speed <=200:
     print("{}, you have sped {}km/h over the limit. You are under arrest and sentenced to 6 months in jail. Have fun talking to Jake Paul!".format(driver, difference))
-
+    list.append(driver)
+    print("The names of all wanted criminals are{}".format(list))
+    
 else:
-    print("{} has been shot by police for speeding {}km/h over the limit.".format(name, difference))
+    print("{} has been shot by the police for speeding {}km/h over the limit.".format(driver, difference))
